484a42ea32ed5befb7e9c674311d8ab06b0ce1f3
parvnirmal57 <parvnirmal57@gmail.com>
2024-12-06 15:03:30 +0530

    final commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SampleComponent } from './sample/sample.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'sample',
+    pathMatch:'full',
+  },
+
+  {
+    path:'sample',
+    component:SampleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/sample/sample.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  templateUrl: './sample.component.html',
+  styleUrls: ['./sample.component.css']
+})
+export class SampleComponent {
+  isLoggedIn = false; //change this to true to see welcome msg
+
+}
